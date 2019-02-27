5bcfc8b636bccfc77a1a8f35a5f0d93ee0c79731
holgern <holgernahrstaedt@gmx.de>
2019-02-27 11:40:07 +0100

    Add description to api methods

--- README.md
+++ README.md
@@ -10,36 +10,49 @@ pip install steemengine
 
 
 ## Commands
+Get the latest block of the sidechain
 ```
 from steemengine.api import Api
 api = Api()
 print(api.getLatestBlockInfo())
 ```
 
+Get the block with the specified block number of the sidechain
 ```
 from steemengine.api import Api
 api = Api()
 print(api.getBlockInfo(1910))
 ```
 
+Retrieve the specified transaction info of the sidechain
+```
+from steemengine.api import Api
+api = Api()
+print(api.getTransactionInfo("e6c7f351b3743d1ed3d66eb9c6f2c102020aaa5d"))
+```
+
+Get the contract specified from the database
 ```
 from steemengine.api import Api
 api = Api()
 print(api.getContract("tokens"))
 ```
 
+Get an array of objects that match the query from the table of the specified contract
 ```
 from steemengine.api import Api
 api = Api()
 print(api.find("tokens", "tokens"))
 ```
 
+Get the object that matches the query from the table of the specified contract
 ```
 from steemengine.api import Api
 api = Api()
 print(api.findOne("tokens", "tokens"))
 ```
 
+Get the transaction history for an account and a token
 ```
 from steemengine.api import Api
 api = Api()
--- steemengine/api.py
+++ steemengine/api.py
@@ -21,6 +21,7 @@ class Api(object):
         self.rpc = RPC()
 
     def get_history(self, account, symbol, limit=1000, offset=0, histtype="user"):
+        """"Get the transaction history for an account and a token"""
         response = ""
         cnt2 = 0
         while str(response) != '<Response [200]>' and cnt2 < 10:
@@ -29,6 +30,7 @@ class Api(object):
         return response.json()
 
     def getLatestBlockInfo(self):
+        """get the latest block of the sidechain"""
         ret = self.rpc.getLatestBlockInfo(endpoint="blockchain")
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
@@ -36,6 +38,7 @@ class Api(object):
             return ret
 
     def getBlockInfo(self, blocknumber):
+        """get the block with the specified block number of the sidechain"""
         ret = self.rpc.getBlockInfo({"blockNumber": blocknumber}, endpoint="blockchain")
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
@@ -43,6 +46,7 @@ class Api(object):
             return ret
 
     def getTransactionInfo(self, txid):
+        """Retrieve the specified transaction info of the sidechain"""
         ret = self.rpc.getTransactionInfo({"txid": txid}, endpoint="blockchain")
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
@@ -50,6 +54,7 @@ class Api(object):
             return ret
 
     def getContract(self, contract_name):
+        """ Get the contract specified from the database"""
         ret = self.rpc.getContract({"name": contract_name}, endpoint="contracts")
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
@@ -57,6 +62,7 @@ class Api(object):
             return ret
 
     def findOne(self, contract_name, table_name, query = {}):
+        """Get the object that matches the query from the table of the specified contract"""
         ret = self.rpc.findOne({"contract": contract_name, "table": table_name, "query": query}, endpoint="contracts")
         if isinstance(ret, list) and len(ret) == 1:
             return ret[0]
@@ -65,6 +71,7 @@ class Api(object):
 
 
     def find(self, contract_name, table_name, query = {}, limit=1000, offset=0, indexes=[]):
+        """Get an array of objects that match the query from the table of the specified contract"""
         ret = self.rpc.find({"contract": contract_name, "table": table_name, "query": query,
                              "limit": limit, "offset": offset, "indexes": indexes}, endpoint="contracts")
         if isinstance(ret, list) and len(ret) == 1:
